f479c47f75609c4b1e283c75606a1c1bcf339c7c
Cassiano Surdio Franco <cassianosfranco@gmail.com>
2020-08-17 16:05:40 -0300

    plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,13 +1,11 @@
-const imbac = require('imba/dist/compiler.js');
+const imbac = require('imba/dist/compiler.js')
 const fs = require('fs')
 
 module.exports = (on) => {
     on('file:preprocessor', async (file) => {
-        var options = {
-            targetPath: file.outputPath.replace(/\.imba\d?$/,'.js')
-        }
         var source = fs.readFileSync(file.filePath, { encoding: 'utf-8'})
-        var compile = await imbac.compile(source, options)
+        var {js, _} = imbac.compile(source, { evaling: true })
+        fs.writeFileSync(file.outputPath + '.js', js)
         return file.outputPath + '.js'
     })
 }
